128dda006def307717a09036055b912421d5d419
cisu <georgrekis@gmail.com>
2022-12-28 19:52:52 +0200

    add error handler

--- server/server.js
+++ server/server.js
@@ -1,4 +1,5 @@
 import fastify from 'fastify';
+import sensible from '@fastify/sensible';
 import dotenv from 'dotenv';
 
 import {PrismaClient} from '@prisma/client';
@@ -6,16 +7,32 @@ import {PrismaClient} from '@prisma/client';
 dotenv.config();
 
 const app = fastify();
+
+// to use sensible
+app.register(sensible);
+
 const prisma = new PrismaClient();
 
 app.get('/posts', async (req, res) => {
   // inside in findMany() that going to get all of the different thing,
-  return await prisma.post.findMany({
-    select: {
-      id: true,
-      title: true,
-    },
-  });
+  return await dbErrorHandler(
+    prisma.post.findMany({
+      select: {
+        id: true,
+        title: true,
+      },
+    })
+  );
 });
+
+// error handler
+async function dbErrorHandler(promise) {
+  // return an error if exist and the data
+  const [error, data] = await app.to(promise);
+
+  if (error) return app.httpErrors.internalServerError(error.message); // 500 error
+
+  return data;
+}
 
 app.listen({port: process.env.PORT});
