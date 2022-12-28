7196d53f22fda3531b1edc485db9489d4ad3f429
cisu <georgrekis@gmail.com>
2022-12-28 18:35:02 +0200

    create a server

--- server/server.js
+++ server/server.js
@@ -1,1 +1,21 @@
 import fastify from 'fastify';
+import dotenv from 'dotenv';
+
+import {PrismaClient} from '@prisma/client';
+
+dotenv.config();
+
+const app = fastify();
+const prisma = new PrismaClient();
+
+app.get('/posts', async (req, res) => {
+  // inside in findMany() that going to get all of the different thing,
+  return await prisma.post.findMany({
+    select: {
+      id: true,
+      title: true,
+    },
+  });
+});
+
+app.listen({port: process.env.PORT});
